777d0a9760d1add969f522c0b33fb6c254f67cce
Mubasher.Skylinx <mubasher.skylinxtech@gmail.com>
2023-02-22 15:50:20 +0500

    refeching data when deleting client

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,6 +2,7 @@ import { useMutation } from "@apollo/client";
 import { FaTrash } from "react-icons/fa";
 import { DELETE_CLIENT } from "../mutations/clientMutations";
 import { GET_CLIENTS } from "../queries/clientQueries";
+import { GET_PROJECTS } from "../queries/projectQueries";
 
 export default function ClientRow(props) {
     const { client } = props;
@@ -9,20 +10,21 @@ export default function ClientRow(props) {
     const [deleteClient] = useMutation(DELETE_CLIENT, {
         variables: { id: client.id },
         // refetchQueries: [{ query: GET_CLIENTS }], // ONE WAY
+        refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }], // ONE WAY
 
-        update(cache, { data: { deleteClient } }) {
-            console.log(deleteClient);
-            const { clients } = cache.readQuery({
-                query: GET_CLIENTS,
-            });
+        // SECOND WAY
+        // update(cache, { data: { deleteClient } }) {
+        //     const { clients } = cache.readQuery({
+        //         query: GET_CLIENTS,
+        //     });
 
-            cache.writeQuery({
-                query: GET_CLIENTS,
-                data: {
-                    clients: clients.filter((client) => client.id !== deleteClient.id),
-                },
-            });
-        },
+        //     cache.writeQuery({
+        //         query: GET_CLIENTS,
+        //         data: {
+        //             clients: clients.filter((client) => client.id !== deleteClient.id),
+        //         },
+        //     });
+        // },
     });
 
     const handleClientDelete = () => {
